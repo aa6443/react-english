b2fcb648af96b326d12adc78882b07d027eea1ee
Aniket Kumar <aa6443@srmist.edu.in>
2025-01-06 19:18:58 +0530

    chore: Add React Router DOM and create basic layout structure

---
+++ 07reactRouter/src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from "react-router-dom"
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+
+export default function Layout() {
+  return (
+    <>
+        <Header />
+        <Outlet />
+        <Footer />
+    
+    </>
+  )
+}
